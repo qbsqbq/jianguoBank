7deed220f286f343881b54c8be9a2b8fbe3ced85
qh <qihao@test.com>
2021-03-30 14:17:37 +0800

    完成一个banar

--- pages/product/product/product.js
+++ pages/product/product/product.js
@@ -1,66 +1,22 @@
-// pages/product/product/product.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+      data: {
+        imgUrls: [{
+          "id": 1,
+          url: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2388864148,4157044652&fm=26&gp=0.jpg'
+        }, {
+          "id": 2,
+          url: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=129474270,2801435851&fm=26&gp=0.jpg'
+        }, {
+          "id": 3,
+          url: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2804989228,4006217075&fm=26&gp=0.jpg'
+        }],
+        indicatorDots: true, //小点
+        indicatorColor: "white", //指示点颜色
+        activeColor: "coral", //当前选中的指示点颜色
+        autoplay: true, //是否自动轮播
+        interval: 2000, //间隔时间
+        duration: 300, //滑动时间
+        circuLar: true, //是否采用衔接滑动
+        easingFunction:"easeInOutCubic", //缓入缓出动画
+      }
+    })
