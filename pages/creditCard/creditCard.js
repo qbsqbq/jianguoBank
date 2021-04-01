b786ef3ec29dc1033f4e377143408954faf5dc17
iOSCong <1876454300@qq.com>
2021-04-01 10:49:26 +0800

    'json数据处理'

--- pages/creditCard/creditCard.js
+++ pages/creditCard/creditCard.js
@@ -1,143 +1,19 @@
 // pages/creditCard/creditCard.js
+
+const jsonData = require('../jsonData/vcuhJson')
+
 Page({
 
   /**
    * 页面的初始数据
    */
-  data: {
-    topList: [
-      {
-        image: "/resources/fu2.png",
-        title: "一键办卡",
-        subTitle: "额度可达5万"
-      },
-      {
-        image: "/resources/fu2.png",
-        title: "我要选卡",
-        subTitle: "达标领拉杆箱等"
-      },
-      {
-        image: "/resources/fu2.png",
-        title: "推荐办卡",
-        subTitle: "抢大牌好礼"
-      }
-    ],
-    itemList: [
-      {
-        meunId: 1,
-        title: "额度",
-        icon: "/resources/produc/xyk.png",
-      },
-      {
-        meunId: 2,
-        title: "查账",
-        icon: "/resources/produc/ck.png",
-      },
-      {
-        meunId: 3,
-        title: "账单分期",
-        icon: "/resources/produc/rmhd.png",
-      },
-      {
-        meunId: 4,
-        title: "现金贷款",
-        icon: "/resources/produc/sbfw.png",
-      },
-      {
-        meunId: 5,
-        title: "掌上商城",
-        icon: "/resources/produc/wdyy.png",
-      },
-      {
-        meunId: 6,
-        title: "卡片精选",
-        icon: "/resources/produc/dk.png",
-      },
-      {
-        meunId: 7,
-        title: "保险服务",
-        icon: "/resources/produc/lccp.png",
-      },
-      {
-        meunId: 8,
-        title: "热门活动",
-        icon: "/resources/produc/jj.png",
-      },
-      {
-        meunId: 9,
-        title: "今日必看",
-        icon: "/resources/produc/cz.png",
-      },
-      {
-        meunId: 10,
-        title: "热议话题",
-        icon: "/resources/produc/qb.png",
-      }
-    ],
-    nowList: [
-      {
-        image: "/resources/fu2.png",
-        title: "春游礼包",
-        date: "截止日期04月30日",
-        havGet: "已申领84392",
-        price: "¥ 290",
-      },
-      {
-        image: "/resources/fu2.png",
-        title: "华为智能体脂秤",
-        date: "截止日期04月02日",
-        havGet: "已申领2984",
-        price: "¥ 2430",
-      }
-    ],
-    scrollOneList: [
-      {
-        image: "/resources/fu2.png",
-        title: "卡超市",
-        subTitle: "丰富卡类,等你来选"
-      },
-      {
-        image: "/resources/fu2.png",
-        title: "一键办卡",
-        subTitle: "轻松办卡,一键获取"
-      }
-    ],
-    gudkList: [
-      {
-        image: "/resources/fu2.png",
-        title: "预借现金",
-        subTitle: "消费专家"
-      },
-      {
-        image: "/resources/fu2.png",
-        title: "账单分期",
-        subTitle: "管账达人"
-      },
-      {
-        image: "/resources/fu2.png",
-        title: "持卡贷款",
-        subTitle: "理财大师"
-      }
-    ],
-    scrollTwoList: [
-      {
-        image: "/resources/fu2.png",
-        title: "重疾保障,边耍边保",
-        subTitle: "刷卡金100%累加保额"
-      },
-      {
-        image: "/resources/fu2.png",
-        title: "免费领取,40万保额",
-        subTitle: "重疾,意外全面保障"
-      }
-    ]
-  },
+  data: jsonData.data,
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
